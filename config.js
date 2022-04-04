1f22f8b002fbbdd7b4ff27f6b41137a5695c952b
HanzBOTZ <100819969+HANZBOTZGZ@users.noreply.github.com>
2022-04-05 02:29:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['https://chat.whatsapp.com/GJQ6KYhcuSZ1lWnCLNTG9T']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+  '6288215110575': { // put your number here
+    name: 'Owner ʜᴀɴᴢ ᴏғᴄ/Creator Botz',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
@@ -13,8 +13,8 @@ global.Owner = {
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
-    name: 'Dave',
+  '6287840949637': { // put your number here
+    name: 'ʜᴀɴᴢ Creator Botz',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
@@ -25,7 +25,7 @@ global.Owner = {
   '13479805233': {} // ignore if you don't want anything, like name dll
 }
 global.mods = ['13479805233'] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['6288215110787','6287840949637'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -52,8 +52,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/6288215110787'
+global.author = 'Subs Hanz OFC'
 
 global.multiplier = 39 // The higher, The harder levelup
 
